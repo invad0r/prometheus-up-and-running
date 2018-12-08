996b29de6a9ae8e0001306ecc30eabb173994991
invad0r <invad0r@gmx.net>
2018-12-08 23:31:25 +0100

    added counter

--- application/metrics.py
+++ application/metrics.py
@@ -1,15 +1,22 @@
 import http.server
 from prometheus_client import start_http_server
+from prometheus_client import Counter
+
+PORT_WEB = 8001
+PORT_PROM = 8000
+
+REQUEST = Counter('hello_worlds_total', 'Hello Worlds requested.')
 
 class MyHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
+        REQUEST.inc()
         self.send_response(200)
         self.end_headers()
         self.wfile.write(b"Hello World\n")
 
 if __name__ == "__main__":
-    print("starting 8000..")
-    start_http_server(8000)
-    print("starting 8001..")
-    server = http.server.HTTPServer(('0.0.0.0', 8001), MyHandler)
+    print("starting %d.." % PORT_PROM)
+    start_http_server(PORT_PROM)
+    print("starting %d.." % PORT_WEB)
+    server = http.server.HTTPServer(('0.0.0.0', PORT_WEB), MyHandler)
     server.serve_forever()
